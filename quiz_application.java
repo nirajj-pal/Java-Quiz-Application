7e9147ad32fa4c3535034810d1a654031032f755
NirajjPal <87762827+NirajjPal@users.noreply.github.com>
2022-03-24 21:25:16 +0530

    Add files via upload

---
+++ quiz_application.java
@@ -0,0 +1,16 @@
+
+public class quiz_application {
+
+//Pinned by Bro Code
+//Bro Code
+//1 year ago
+// ---------------------------- Main ----------------------------
+
+	public static void main(String[] args) {
+		
+		Quiz quiz = new Quiz();
+	}
+}
+// ---------------------------- Quiz ----------------------------
+
+
